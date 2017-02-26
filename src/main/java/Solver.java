75ba09102d7513980dc0218e28bfd78e9c96cad1
Bence2 <lammelbence@gmail.com>
2017-02-26 12:40:59 +0100

    solver in almost final state, checking the test data

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -3,39 +3,106 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
+    
+    private boolean isSolvable;
+    private Stack<Board> solutionSteps;
+    private int minNumberOfMoves;
 
+//    public static void main(String[] args) {
+//        int demoLength = 3;
+//        int[][] blocksP = new int[demoLength][demoLength];
+//        int value = 1;
+//        for (int i = 0; i < blocksP.length; i++) {
+//            for (int j = 0; j < blocksP.length; j++) {
+//                blocksP[i][j] = value;
+//                value++;
+//            }
+//        }
+//        blocksP[demoLength-1][demoLength-1] = 1;
+//        blocksP[0][0] = 0;
+////        int tmpValue = blocksP[demoLength-1][demoLength-1];
+////        blocksP[demoLength-1][demoLength-1] = blocksP[demoLength-1][demoLength-1]
+//        Solver solver = new Solver(new Board(blocksP));
+//
+//    }
+    
     public static void main(String[] args) {
-        int demoLength = 3;
-        int[][] blocksP = new int[demoLength][demoLength];
-        int value = 1;
-        for (int i = 0; i < blocksP.length; i++) {
-            for (int j = 0; j < blocksP.length; j++) {
-                blocksP[i][j] = value;
-                value++;
-            }
-        }
-        blocksP[demoLength-1][demoLength-1] = 1;
-        blocksP[0][0] = 0;
-//        int tmpValue = blocksP[demoLength-1][demoLength-1];
-//        blocksP[demoLength-1][demoLength-1] = blocksP[demoLength-1][demoLength-1]
-        Solver solver = new Solver(new Board(blocksP));
 
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
     }
     
     public Solver(Board initialBoardP) {
-        getSolution(initialBoardP);
+        BoardWrapper finalBoardWrapper = getSolution(initialBoardP);
+        this.solutionSteps = fillSolutionSteps(finalBoardWrapper);
+        this.isSolvable = finalBoardWrapper.getBoard().isGoal();
+        this.minNumberOfMoves = finalBoardWrapper.getNumberOfMoves();
+    }
+    
+    private Stack<Board> fillSolutionSteps(BoardWrapper finalBoardWrapper) {
+        if (finalBoardWrapper.getBoard().isGoal()) {
+            solutionSteps = new Stack<>();
+            solutionSteps.push(finalBoardWrapper.getBoard());
+            
+            while (finalBoardWrapper.getParentBoard() != null) {
+                solutionSteps.push(finalBoardWrapper.getParentBoard().getBoard());
+            }
+            return solutionSteps;
+        }
+        else {
+            return null;
+        }
+    }
+    
+    public Iterable<Board> solution() {
+        return solutionSteps;
+    }
+    
+    public int moves() {
+        if (!this.isSolvable) {
+            return -1;
+        }
+        else {
+            return this.minNumberOfMoves;
+        }
         
     }
     
-    private void getSolution(Board initialBoardP) {
-        MinPQ<BoardWrapper> boardOriginalPQ = new MinPQ<>(new BoardWrapperComparatorHamming());
+    public boolean isSolvable() {
+        return isSolvable;
+    }
+    
+    private BoardWrapper getSolution(Board initialBoardP) {
+        MinPQ<BoardWrapper> boardOriginalPQ = new MinPQ<>(new BoardWrapperComparatorManhattan());
         BoardWrapper initialOriginalBoardWrapper = new BoardWrapper(initialBoardP, null);
         initialOriginalBoardWrapper.setNumberOfMoves(0);
         
-        MinPQ<BoardWrapper> boardTwinPQ = new MinPQ<>(new BoardWrapperComparatorHamming());
+        MinPQ<BoardWrapper> boardTwinPQ = new MinPQ<>(new BoardWrapperComparatorManhattan());
         Board initialTwinBoard = initialBoardP.twin();
         BoardWrapper initialTwinBoardWrapper = new BoardWrapper(initialTwinBoard, null);
         initialTwinBoardWrapper.setNumberOfMoves(0);
@@ -50,7 +117,7 @@ public class Solver {
             initialTwinBoardWrapper = getSolutionStep(initialTwinBoardWrapper, boardTwinPQ);
             isTwinBoardSolved = initialTwinBoardWrapper.getBoard().isGoal();
         }
-        System.out.println("sikeer");
+        return initialOriginalBoardWrapper;
     }
     
     private BoardWrapper getSolutionStep(BoardWrapper currentBoardWrapper, MinPQ<BoardWrapper> boardPQ) {
