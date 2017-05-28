42fdb5795c5fc2393f533f51a31e88e37e722c6f
Bence2 <lammelbence@gmail.com>
2017-05-28 23:49:33 +0200

    KdTree insert megvan

--- src/main/java/KdTree.java
+++ src/main/java/KdTree.java
@@ -8,24 +8,36 @@ public class KdTree {
     private Node rootNode;
     
     public static void main(String[] args) {
+        KdTree kdtree = new KdTree();
+        Point2D point1 = new Point2D(0.2, 0.5);
+        Point2D point2 = new Point2D(0.1, 0.5);
+        Point2D point3 = new Point2D(0.1, 0.7);
+        Point2D pointNemLetezo = new Point2D(0.1, 0.1);
+        kdtree.insert(point1);
+        kdtree.insert(point2);
+        kdtree.insert(point3);
+        System.out.println(kdtree.contains(point3));
+        System.out.println(kdtree.contains(pointNemLetezo));
+        System.out.println("vege");
     }
     
     public void insert(Point2D point) {
         // if such point exists update, otherwise insert a new node
         checkInputData(point);
-        
-        
-    }
-    
-    public boolean contains(Point2D point) {
-        checkInputData(point);
+        if (rootNode == null) {
+            rootNode = new Node(point);
+            return;
+        }
         Node currentNode = rootNode;
         Point2D currentPoint = rootNode.getPoint();
-        boolean isContained = false;
         int treeDepthCounter = 0;
         Comparator<Point2D> comparator;
-        while (currentNode != null) {
-            if ((treeDepthCounter % 2) == 0) {
+        while (true) {
+            if (point.equals(currentPoint)) {
+                currentNode.setPoint(point);
+                break;
+            }
+            if ((treeDepthCounter++ % 2) == 0) {
                 comparator = point.X_ORDER;
             } 
             else {
@@ -34,15 +46,54 @@ public class KdTree {
             
             int comparisonResult = comparator.compare(point, currentPoint);
             if (comparisonResult == -1) {
+                if (currentNode.getLeftNode() != null) {
+                    currentNode = currentNode.getLeftNode();
+                    currentPoint = currentNode.getPoint();
+                }
+                else {
+                    currentNode.setLeftNode(new Node(point));
+                    break;
+                }
+            }
+            else if (comparisonResult == 0 || comparisonResult == 1) {
+                if (currentNode.getRightNode() != null) {
+                    currentNode = currentNode.getRightNode();
+                    currentPoint = currentNode.getPoint();
+                }
+                else {
+                    currentNode.setRightNode(new Node(point));
+                    break;
+                }
+            }
+        }
+        
+    }
+    
+    public boolean contains(Point2D point) {
+        checkInputData(point);
+        Node currentNode = rootNode;
+        int treeDepthCounter = 0;
+        Comparator<Point2D> comparator;
+        while (currentNode != null) {
+            if (point.equals(currentNode.getPoint())) {
+                return true;
+            }
+            if ((treeDepthCounter % 2) == 0) {
+                comparator = point.X_ORDER;
+            } 
+            else {
+                comparator = point.Y_ORDER;
+            }
+            
+            int comparisonResult = comparator.compare(point, currentNode.getPoint());
+            if (comparisonResult == -1) {
                 currentNode = currentNode.getLeftNode();
-                currentPoint = currentNode.getPoint();
             }
             else if (comparisonResult == 0 || comparisonResult == 1) {
                 currentNode = currentNode.getRightNode();
-                currentPoint = currentNode.getPoint();
             }
         }
-        return isContained;
+        return false;
     }
     
     
@@ -60,6 +111,9 @@ public class KdTree {
         
         public Node() {
             
+        }
+        public Node(Point2D point) {
+            this.point = point;
         }
         
         public Point2D getPoint() {
